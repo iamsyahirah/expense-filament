5d17448e42b355dcf3e8aa3b03e42a921e24470e
iamsyahirah <syahirahmohammad89@gmail.com>
2024-09-09 21:53:04 +0800

    feat: filter dashboard

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,16 +2,27 @@
 
 namespace App\Filament\Widgets;
 
+use Carbon\Carbon;
 use App\Models\Transaction;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class StatsOverview extends BaseWidget
 {
+    use InteractsWithPageFilters;
     protected function getStats(): array
     {
-        $income = Transaction::incomes()->get()->sum('amount');
-        $expense = Transaction::expenses()->get()->sum('amount');
+        $startDate = !is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = !is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
+        $income = Transaction::incomes()->get()->whereBetween('date_transaction', [$startDate, $endDate])->sum('amount');
+        $expense = Transaction::expenses()->get()->whereBetween('date_transaction', [$startDate, $endDate])->sum('amount');
         return [
             Stat::make('Total Expense', $expense),
             Stat::make('Total Income', $income),
--- app/Filament/Widgets/WidgetExpenseChart.php
+++ app/Filament/Widgets/WidgetExpenseChart.php
@@ -2,18 +2,29 @@
 
 namespace App\Filament\Widgets;
 
-use App\Models\Transaction;
+use Carbon\Carbon;
 use Flowframe\Trend\Trend;
+use App\Models\Transaction;
 use Flowframe\Trend\TrendValue;
 use Filament\Widgets\ChartWidget;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 
 class WidgetExpenseChart extends ChartWidget
 {
+    use InteractsWithPageFilters;
     protected static ?string $heading = 'Expense';
     protected static string $color = 'danger';
 
     protected function getData(): array
     {
+        $startDate = !is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = !is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
         $data = Trend::query(Transaction::expenses())
             ->between(
                 start: now()->startOfYear(),
